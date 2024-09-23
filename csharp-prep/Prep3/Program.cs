9865924afedf57195bc9ac3757d82e82c6580d09
Nathan Reynolds <rey21031@byui.edu>
2024-09-23 13:50:57 -0600

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+    
+        int answer = 18;
+        int guess = 0;
+        do
+        {
+            Console.Write("What's the magic number?");
+            guess = int.Parse(Console.ReadLine());
+            if (guess < answer)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > answer)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+        while (guess != answer);
+        
+        
+
+        
+
+
     }
 }
