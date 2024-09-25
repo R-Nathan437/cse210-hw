cb7167f9595cb547dad1f6434a1e853c394fbb0f
Nathan Reynolds <rey21031@byui.edu>
2024-09-25 13:37:04 -0600

    strecth prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,41 +1,43 @@
 using System;
 using System.Formats.Asn1;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-     Random randomGenerator = new Random();
-        int answer = randomGenerator.Next(1, 11);
-        int guess = 0;
-        int count = 0;
-        do
+        Console.WriteLine("Would you like to play?");
+        string answer_continue = Console.ReadLine();
+        while (answer_continue is "yes")
         {
-            Console.Write("What's the magic number?");
-            guess = int.Parse(Console.ReadLine());
-            if (guess < answer)
+            Random randomGenerator = new Random();
+            int answer = randomGenerator.Next(1, 11);
+            int guess = 0;
+            int count = 0;
+            do
             {
-                count += 1;
-                Console.WriteLine("Higher");
+                Console.Write("What's the magic number?");
+                guess = int.Parse(Console.ReadLine());
+                if (guess < answer)
+                {
+                    count += 1;
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > answer)
+                {
+                    count += 1;
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    count += 1;
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"{count} guess(es)");
+                }
             }
-            else if (guess > answer)
-            {
-                count += 1;
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                count += 1;
-                Console.WriteLine("You guessed it!");
-                Console.WriteLine($"{count} guesse(s)");
-            }
-        }
         while (guess != answer);
-        
-        
-
-        
-
-
+        Console.WriteLine("Would you like to play again");
+        answer_continue = Console.ReadLine();
+        }
     }
 }
