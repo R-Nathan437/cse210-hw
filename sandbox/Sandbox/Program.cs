c225a495afe30e19ada8f91e1319fd5a22cee4ef
Nathan Reynolds <rey21031@byui.edu>
2024-09-25 13:09:10 -0600

    class example

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
+        List<double> x;
+        x = [24.0,25.0,26.0];
+        foreach (double number in x)
+        {
+            Console.WriteLine(number);
+        }
+
         Console.WriteLine("Hello Sandbox World!");
     }
 }
