fec3127750f336ad65dfd4251e628b3dded491f6
Nathan Reynolds <rey21031@byui.edu>
2024-09-25 13:09:46 -0600

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,24 +5,29 @@ class Program
 {
     static void Main(string[] args)
     {
-    
-        int answer = 18;
+     Random randomGenerator = new Random();
+        int answer = randomGenerator.Next(1, 11);
         int guess = 0;
+        int count = 0;
         do
         {
             Console.Write("What's the magic number?");
             guess = int.Parse(Console.ReadLine());
             if (guess < answer)
             {
+                count += 1;
                 Console.WriteLine("Higher");
             }
             else if (guess > answer)
             {
+                count += 1;
                 Console.WriteLine("Lower");
             }
             else
             {
+                count += 1;
                 Console.WriteLine("You guessed it!");
+                Console.WriteLine($"{count} guesse(s)");
             }
         }
         while (guess != answer);
