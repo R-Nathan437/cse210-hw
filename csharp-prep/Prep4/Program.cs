f42a790c962eac03bd056e321a389a17a17d045b
Nathan Reynolds <rey21031@byui.edu>
2024-09-27 13:46:38 -0600

    prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int newNumber = -1;
+        Console.WriteLine("Enter a number (0 to quit):");
+        while(newNumber != 0)
+        
+            {
+                Console.Write("Enter a number:");
+                newNumber = int.Parse(Console.ReadLine());
+                if(newNumber != 0)
+                {
+                    numbers.Add(newNumber);
+                }
+            }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is {sum}");
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[^1];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
     }
 }
