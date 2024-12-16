1adb0f08c5e88dc2b959291fd86f7a6ad72784b7
Nathan Reynolds <rey21031@byui.edu>
2024-12-15 19:31:02 -0700

    foundation 1

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        List<Video> _videos = new List<Video>();
+        Video v1 = new Video("title", "120", "author");
+        v1.addComment("author", "text");
+        _videos.Add(v1);
+        foreach (Video video in _videos)
+        {
+            video.Display();
+        }
+
     }
 }
