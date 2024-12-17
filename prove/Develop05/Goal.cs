3fac6483d050d7b74aa8c953b0f85bfafc953c0c
Nathan Reynolds <rey21031@byui.edu>
2024-12-17 03:58:22 -0700

    Prove 5

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -1,20 +1,46 @@
 using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Transactions;
 
 public abstract class Goal
 {
     protected string _title;
+    public string title
+    {
+        get{return _title;}
+        set{_title = value;}
+    }
     protected string _description;
-    protected int _points;
+    public string description
+    {
+        get{return _description;}
+        set{description = value;}
+    }
+    protected int _goalPoints;
+    public int goalPoints
+    {
+        get{return _goalPoints;}
+        set{_goalPoints = value;}
+    }
     protected string _type;
+    public string type
+    {
+        get{return _type;}
+        set{_type = value;}
+    }
     protected bool _status;
-    protected List<Goal> goals;
+    public bool status
+    {
+        get{return _status;}
+        set{_status = value;}
+    }
     public Goal()
         {
             _title = "";
             _description = "";
-            _points = 0;
+            _goalPoints = 0;
+            _type = "";
              _status = false;
         }
     
@@ -23,48 +49,40 @@ public abstract class Goal
     {
         _title = title;
         _description = description;
-        _points = points;
+        _goalPoints = points;
         _type = type;
         _status = false;
     }
-    public void DisplayPoints()
+    public virtual void creatOutline(string Type)
     {
-        Console.WriteLine($"{_points} points\n");
-    }
-    protected void creatOutline()
-    {
-        Console.Write.("Goal name: ");
+        Console.Write("Goal name: ");
         _title = Console.ReadLine();
-        Console.Write.("Goal description:");
+        Console.Write("Goal description:");
         _description = Console.ReadLine();
-        Console.Write.("Points: ");
+        Console.Write("Points: ");
         string stringPoints = (Console.ReadLine());
-
-        if (int.TryParse(stringPoints, out int points))
+        bool incorrect = true;
+        while(incorrect)
         {
-            _points = points;
-        }
-        else
-        {
-            //invaild
-        }
-        _status = false;
-    }
-    public void save(string filename)
-    {
-        using (StreamWriter outputFile = new StreamWriter(filename, true))
-        {
-            foreach (Goal goal in goals)
+            if (int.TryParse(stringPoints, out int points))
             {
-                outputFile.WriteLine(goal);
+                _goalPoints = points;
+                incorrect = false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid. Try again.\nPoints:");
+                stringPoints = Console.ReadLine();
             }
         }
-
     }
-
-    public abstract int GetPoints();
-    
+    public abstract void DisplayGoal();
+    public abstract int AddPoints();
     public abstract void RecordGoal();
-    
+    public virtual bool IsComplete()
+    {
+        return _status;
+    }
+    public abstract string Save();
     
 }
