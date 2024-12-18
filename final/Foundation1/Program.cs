a6756ed7bbafc912fe56b12570d82bb84234bcfe
Nathan Reynolds <rey21031@byui.edu>
2024-12-18 07:24:25 -0700

    Foundation 2

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -6,9 +6,14 @@ class Program
     static void Main(string[] args)
     {
         List<Video> _videos = new List<Video>();
-        Video v1 = new Video("title", "120", "author");
-        v1.addComment("author", "text");
+        Video v1 = new Video("title1", "120", "author1");
+        Video v2 = new Video("title2", "10", "author2");
+        v1.addComment("author1", "text1");
+        v2.addComment("author1", "text1");
+        v2.addComment("author2", "text2");
+
         _videos.Add(v1);
+        _videos.Add(v2);
         foreach (Video video in _videos)
         {
             video.Display();
